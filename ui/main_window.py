51ffda2937ccb1d519786291a310505ad6e7fe86
Nikolay Gerginov <123015737+nklgerginov@users.noreply.github.com>
2026-08-03 18:54:07 +0300

    Fix: Merge split setWindowTitle string literal

--- ui/main_window.py
+++ ui/main_window.py
@@ -56,10 +56,7 @@ class ExportThread(QThread):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Video Editor - Highlighted Subt
-itles"
-)
-        self.setMinimumSize(1200, 800)
+        self.setWindowTitle("Video Editor - Highlighted Subtitles")        self.setMinimumSize(1200, 800)
         self.project = VideoProject()
         self.current_video_path = ""
         self.current_time = 0.0
@@ -108,12 +105,12 @@ itles"
         vosk_layout.addWidget(QLabel("Select Vosk Model:"))
         vosk_layout.addWidget(self.model_combo)
         vosk_group.setLayout(vosk_layout)
-
-        right_panel.addWidget(vosk_group)
+      
+  right_panel.addWidget(vosk_group)
 
         self.process_button = QPushButton("Generate Subtitles")
-
-        self.process_button.setEnabled(False)
+ 
+       self.process_button.setEnabled(False)
         right_panel.addWidget(self.process_button)
 
         self.processing_label = QLabel("Processing...")
@@ -153,8 +150,8 @@ itles"
         position_layout.
 addWidget(self.save_position_button)
         self.position_label = QLabel("Position: 50px, 50px")
-
-        position_layout.addWidget(self.position_label)
+   
+     position_layout.addWidget(self.position_label)
 
         position_form = QFormLayout()
         self.x_spin = QSpinBox()
@@ -201,8 +198,8 @@ addWidget(self.save_position_button)
         self.font_size_spin.valueChanged.conn
 ect(self._update_style)
         self.highlight_scale_spin.valueChanged.connect(self._update_style)
-
-        self.text_color_button.clicked.connect(self._choose_text_color)
+ 
+       self.text_color_button.clicked.connect(self._choose_text_color)
         self.highlight_color_button.clicked.connect(self._choose_highlight_color)
         self.save_position_button.clicked.connect(self._save_pos)
         self.x_spin.valueChanged.connect(self._update_pos)
@@ -243,7 +240,7 @@ ect(self._update_style)
             highlight_color=self.highlight_color.name()
         )
         if self.preview_widget.project:
-
+            
 self.preview_widget.set_project(self.project)
 
     def _choose_text_color(self):
@@ -294,7 +291,7 @@ olorDialog.getColor(self.text_color, self, "Choose Text Color")
             return
         if self.media_player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
             return
-
+        
 self.media_player.play()
 
     def _pause(self):
@@ -349,8 +346,8 @@ elf):
         self.exp_thread.export_complete.connect(self._on_exp_d
 one)
         self.exp_thread.error_occurred.connect(self._on_exp_err)
-
-            self.exp_thread.start()
+       
+ self.exp_thread.start()
 
     def _on_exp_done(self, path):
         self.export_label.setVisible(False)
