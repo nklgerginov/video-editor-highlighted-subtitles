38df551eaf0abc7ac95b052081f3a66c9cbfac1d
vo1d-null <vo1d.null299@gmail.com>
2026-08-03 14:37:17 +0300

    ---

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
 import sys
 import os
-<<<<<<< HEAD
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 if project_dir not in sys.path:
@@ -10,14 +8,5 @@ if project_dir not in sys.path:
 
 from ui.main_window import main
 
-=======
-=======
-import sys, os
->>>>>>> d68708e919fc1d12be8141077fdbfdeb7ddd243c
-project_dir = os.path.dirname(os.path.abspath(__file__))
-if project_dir not in sys.path:
-    sys.path.insert(0, project_dir)
-from ui.main_window import main
->>>>>>> a71566016695e21c407a34efabef2157bae5f31d
 if __name__ == "__main__":
     main()
